5f35ee11a273023e79b43f16e890314c7ad5a0ae
578784139 <578784139@qq.com>
2017-08-11 14:04:04 +0800

    修改班级名称自增表

--- src/main/java/cn/bdqn/tangcco/lookwell/entity/CourseArranging.java
+++ src/main/java/cn/bdqn/tangcco/lookwell/entity/CourseArranging.java
@@ -14,6 +14,7 @@ public class CourseArranging {
     private Chapter chapter;
     private Date arrangingDate;
 
+
     @Override
     public String toString() {
         return "CourseArranging{" +
